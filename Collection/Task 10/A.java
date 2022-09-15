028c7ff48f7546043902d833f49db969a6bba8b4
mayur45-git <112369875+mayur45-git@users.noreply.github.com>
2022-09-15 15:27:13 +0530

    Collection

---
+++ Collection/Task 10/A.java
@@ -0,0 +1,15 @@
+package com.yash.collection.Task_10;
+
+public class A {
+	
+	int data;
+	public A backward;
+	public A forward;
+	 
+	 A(int data)
+	 {
+	 this.data = data;
+	 backward = forward = null;
+	 }
+
+}
