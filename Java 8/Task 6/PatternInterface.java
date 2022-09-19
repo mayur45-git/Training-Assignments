ddb0d5858e877dd67f32076a799b962a9c8d302f
mayur45-git <112369875+mayur45-git@users.noreply.github.com>
2022-09-19 13:16:27 +0530

    Delete Java 8 directory

--- Java 8/Task 6/PatternInterface.java
+++
@@ -1,7 +0,0 @@
-package com.yash.Task_6;
-
-public interface PatternInterface {
-
-	
-	void displayPattern(int row);
-}
--- Java 8/Task 7/Cal.java
+++
@@ -1,7 +0,0 @@
-package com.yash.Task_7;
-
-public interface Cal {
-	
-	 public double calculate(double principal, double rate, double time, double emi);
-
-}
